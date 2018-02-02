5f17ac9dd5357c581fbc2f6b551cf78e3f46f1f6
Ervin Zhou <ervin.zhou@hotmail.com>
2018-02-02 23:33:07 +1100

    Simple, non-resizable go gui
    Non-resizable for simplicity and quick development.

--- go_gui.py
+++ go_gui.py
@@ -7,24 +7,19 @@ pygame.init()
 
 def main():
 	#CONSTANTS
-	WINWIDTH = 400
-	WINHEIGHT = 300
+	WINWIDTH = 800
+	WINHEIGHT = 600
 
 	#set up display surface
-	DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT), pygame.RESIZABLE)
+	DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
 	pygame.display.set_caption("GO")
 	board_img = pygame.image.load("go_board.png").convert()
+	board_img = pygame.transform.scale(board_img, (WINWIDTH, WINHEIGHT))
 	DISPLAYSURF.blit(board_img, (0, 0))
 
 	while True:
 		for event in pygame.event.get():
-			if event.type == VIDEORESIZE:
-				WINWIDTH, WINHEIGHT = event.size
-				board_img = pygame.image.load("go_board.png").convert()
-				board_img = pygame.transform.scale(board_img, event.size)
-				DISPLAYSURF = pygame.display.set_mode(event.size, pygame.RESIZABLE)
-				DISPLAYSURF.blit(board_img, (0, 0))
-			elif event.type == QUIT:
+			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
 
