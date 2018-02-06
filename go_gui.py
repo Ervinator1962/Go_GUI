c5c7c44f24dd8fe901e39ebb99fae7794befb34b
Ervin Zhou <ervin.zhou@hotmail.com>
2018-02-07 05:17:48 +1100

    Functional Go GUI
    1st working prototype of go gui.
    Has capture detection and removal, prevent placement of stone on another stone, and working correct stone position placement.

--- go_gui.py
+++ go_gui.py
@@ -4,111 +4,150 @@ from math import sqrt
 
 pygame.init()
 
-def main():
-	#CONSTANTS
-	WINWIDTH = 650
-	WINHEIGHT = 650
-	STONEHEIGHT = 24
-	STONEWIDTH = 24
-	STONEWHITEX = None
-	STONEWHITEY = None
-	STONEWHITE = pygame.image.load("white_stone.png")
-	STONEWHITE = pygame.transform.scale(STONEWHITE, (STONEWIDTH, STONEHEIGHT))
+#GLOBAL CONSTANTS
+FPS = 30
+WINWIDTH = 650
+WINHEIGHT = 650
+BOARDMARGIN = 20
+BOXWIDTH = 34
+STONEHEIGHT = 24
+STONEWIDTH = 24
 
-	#set up display surface
-	global DISPLAYSURF
-	DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
-	pygame.display.set_caption("GO")
-	board_img = pygame.image.load("go_board.png").convert()
-	board_img = pygame.transform.scale(board_img, (WINWIDTH, WINHEIGHT))
-	DISPLAYSURF.blit(board_img, (0, 0))
+#set up display surface and FPS clock
+global DISPLAYSURF, FPSCLOCK
+DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
+pygame.display.set_caption("GO")
+FPSCLOCK = pygame.time.Clock()
+
+#set up player stones and board
+STONEWHITE = pygame.image.load("white_stone.png").convert_alpha()
+STONEWHITE = pygame.transform.scale(STONEWHITE, (STONEWIDTH, STONEHEIGHT))
+STONEBLACK = pygame.image.load("black_stone.png").convert_alpha()
+STONEBLACK = pygame.transform.scale(STONEBLACK, (STONEWIDTH, STONEHEIGHT))
+BOARDIMG = pygame.image.load("go_board.png").convert()
+BOARDIMG = pygame.transform.scale(BOARDIMG, (WINWIDTH, WINHEIGHT))
+
+def main():
+	#set up cursor
+	cursorX = None
+	cursorY = None
+
+	#set up players
+	black = Player("black")
+	white = Player("white")
+
+	#set up game board data structure
+	game_board = [["-" for y in range(19)] for x in range(19)]
+
+	show_board(game_board)
+
+	roundnum = 0
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == MOUSEMOTION:
-				STONEWHITEX, STONEWHITEY = event.pos
-				STONEWHITEX -= STONEWIDTH / 2
-				STONEWHITEY -= STONEHEIGHT / 2
-				DISPLAYSURF.blit(board_img, (0, 0))
-				#DISPLAYSURF.blit(STONEWHITE, (STONEWHITEX, STONEWHITEY))
+				cursorX, cursorY = event.pos
+				cursorX -= STONEWIDTH / 2
+				cursorY -= STONEHEIGHT / 2
+				show_board(game_board)
+				if roundnum % 2 == 0:
+					DISPLAYSURF.blit(STONEBLACK, (cursorX, cursorY))
+				else:
+					DISPLAYSURF.blit(STONEWHITE, (cursorX, cursorY))
 			elif event.type == MOUSEBUTTONDOWN:
-				place_stone(event.pos)
+				res = None
+				if roundnum % 2 == 0:
+					res = place_stone(game_board, event.pos, black)
+				else:
+					res = place_stone(game_board, event.pos, white)
+				# TODO: Possible unwanted roundnum advances caused by multiple mousedown events
+				if res == 1:	
+					roundnum += 1
+					show_board(game_board)
 				#print(event.pos)
-				pass
+			elif event.type == KEYDOWN:
+				if event.key == "K_ESCAPE":
+					# TODO: Possible unwanted roundnum advances caused by multiple mousedown events
+					roundnum += 1
+					break
 			elif event.type == QUIT:
 				pygame.quit()
 				sys.exit()
 
 		pygame.display.update()
+		FPSCLOCK.tick(FPS)
 
-# extra row for x-axis coordinates
-game_board = [["+" for x in range(20)] for y in range(20)]
-for i in range(len(game_board) - 1):
-	game_board[i][19] = i
-for i in range(len(game_board[0]) - 1):
-	game_board[19][i] = i
-game_board[19][19] = ""
 
-def place_stone(pos):
+def place_stone(board, pos, player):
 	# TODO: MAKE STONEWIDTH AND STONEHEIGHT GLOBAL
-	STONEHEIGHT = 24
-	STONEWIDTH = 24
-	BOARDMARGIN = 20
-	BOXWIDTH = 35
+	# TODO: 34 as box width throws off position of stones further from the top left corner. Maybe reset and recalibrate when past halfway.
 	LIMITRADIUS = BOXWIDTH / 2
 	posx, posy = pos
 	posx -= BOARDMARGIN
 	posy -= BOARDMARGIN
-	if posx < 0:
-		posx = 0
-	if posy < 0:
-		posy = 0
 	#round to nearest int
-	rownum = int(posy / BOXWIDTH + 0.5)
 	colnum = int(posx / BOXWIDTH + 0.5)
+	rownum = int(posy / BOXWIDTH + 0.5)
+	if colnum < 0:
+		colnum = 0
+	if colnum > 18:
+		colnum = 18
+	if rownum < 0:
+		rownum = 0
+	if rownum > 18:
+		rownum = 18
 	valid_pos = (colnum * BOXWIDTH + BOARDMARGIN, rownum * BOXWIDTH + BOARDMARGIN)
 	DISTANCEFROMVALIDPOS = sqrt(pow(valid_pos[0] - pos[0], 2) + pow(valid_pos[1] - pos[1], 2)) 
-	print(DISTANCEFROMVALIDPOS)
+	#print(DISTANCEFROMVALIDPOS)
 	if DISTANCEFROMVALIDPOS < LIMITRADIUS:
+		#print("testing is stone")
+		if is_stone(board, (colnum, rownum)) is False:
 		#print("Acceptable distance")
-		newstone = pygame.image.load("white_stone.png")
-		newstone = pygame.transform.scale(newstone, (STONEWIDTH, STONEHEIGHT))
-		DISPLAYSURF.blit(newstone, valid_pos)
-	else:
-		pass
-		#print("Invalid distance")
-	return (rownum, colnum)
-	#print("pos =", valid_pos)
+		#print(valid_pos)
+			player.make_move(board, (colnum, rownum))
+			return 1
+	return 0
+	# TODO: On invalid postion, make a reprompt function or something
+	# return 0
 
-def print_board(board):
-	print("\n\n\n")
+def show_board(board):
+	DISPLAYSURF.blit(BOARDIMG, (0, 0))
 	for i in range(len(board)):
 		for j in range(len(board[0])):
-			print("{:<4}".format(board[i][j]), end="")
-		print("\n")
+			posx = BOARDMARGIN + BOXWIDTH * i
+			posy = BOARDMARGIN + BOXWIDTH * j
+			posx -= STONEWIDTH / 2
+			posy -= STONEHEIGHT / 2
+			if board[i][j] == "@":
+				DISPLAYSURF.blit(STONEBLACK, (posx, posy))
+			elif board[i][j] == "O":
+				DISPLAYSURF.blit(STONEWHITE, (posx, posy))
+
 
 
 def on_board(pos):
+	#print("pos[0] =", pos[0])
 	return 0 <= pos[0] <= 18 and 0 <= pos[1] <= 18
 
 
-def is_stone(pos, board):
+def is_stone(board, pos):
 	assert on_board(pos)
+	#print(pos, board[pos[0]][pos[1]])
 	return board[pos[0]][pos[1]] == "@" or board[pos[0]][pos[1]] == "O"
 
 
-def same_colour(pos1, pos2, board):
-	assert is_stone(pos1, board), "pos1 is not a valid stone"
-	assert is_stone(pos2, board), "pos2 is not a valid stone"
+def same_colour(board, pos1, pos2):
+	assert is_stone(board, pos1), "pos1 is not a valid stone"
+	assert is_stone(board, pos2), "pos2 is not a valid stone"
 	return board[pos1[0]][pos1[1]] == board[pos2[0]][pos2[1]]
 
 
-def remove_stones(coordinate_list, board, stones_played_list):
+def remove_stones(board, coordinate_list, stones_played_list):
 	# TODO: Check to see if stones_played_list is working properly
-	for (row, col) in coordinate_list:
+	for (col, row) in coordinate_list:
 		# indicate the current round's captured stones with '.'
-		board[row][col] = "."
-		stones_played_list.remove((row, col))
+		board[col][row] = "."
+		stones_played_list.remove((col, row))
 	return len(coordinate_list)
 
 
@@ -121,31 +160,31 @@ def remove_capture_indicators(board):
 
 
 # TODO: Check stones_played_list
-def check_liberties(pos, board, stones_played_list):
+def check_liberties(board, pos, stones_played_list):
 	assert on_board(pos) is True, "Position does not lie on the board"
-	# print("New src = %d %d" % (row, col))
-	if is_stone(pos, board) is False:
+	# print("New src = %d %d" % (col, row))
+	if is_stone(board, pos) is False:
 		return 0
 	visited = [pos]
-	has_liberties = is_free(pos, board, visited)
+	has_liberties = is_free(board, pos, visited)
 	# print("is_free = %s" % str(has_liberties))
 	# print("visited =", visited)
 	num_removed = 0
 	if has_liberties is False:
 		# print("is_free must be false: is_free = %s" % str(has_liberties))
-		num_removed = remove_stones(visited, board, stones_played_list)
+		num_removed = remove_stones(board, visited, stones_played_list)
 	# print("Num removed == %d" % num_removed)
 	return num_removed
 
 
-def is_free(pos, board, visited):
+def is_free(board, pos, visited):
 	has_liberties = False
 	# print("New call")
 	# print("visited = ", visited)
-	pos_above = (pos[0] - 1, pos[1])
-	pos_below = (pos[0] + 1, pos[1])
-	pos_left = (pos[0], pos[1] - 1)
-	pos_right = (pos[0], pos[1] + 1)
+	pos_above = (pos[0], pos[1] - 1)
+	pos_below = (pos[0], pos[1] + 1)
+	pos_left = (pos[0] - 1, pos[1])
+	pos_right = (pos[0] + 1, pos[1])
 	positions = [pos_above, pos_below, pos_left, pos_right]
 	positions_iter = list(positions)
 
@@ -153,50 +192,19 @@ def is_free(pos, board, visited):
 	for adj_pos in positions_iter:
 		if on_board(adj_pos) is False:
 			positions.remove(adj_pos)
-		elif is_stone(adj_pos, board) is False:
+		elif is_stone(board, adj_pos) is False:
 			# if pos is on the board and does not hold stone then it is a liberty
 			has_liberties = True
 			return has_liberties
 
 	for adj_pos in positions:
-		if same_colour(pos, adj_pos, board) and adj_pos not in visited:
+		if same_colour(board, pos, adj_pos) and adj_pos not in visited:
 			visited.append(adj_pos)
-			# input("pos: %d %d" % (row - 1, col))
-			has_liberties = is_free(adj_pos, board, visited)
+			has_liberties = is_free(board, adj_pos, visited)
 			if has_liberties is True:
 				break
 
 	return has_liberties
-
-
-def num_strings(text):
-	num_spaces = 0
-	for letter in text:
-		if letter == " ":
-			num_spaces += 1
-
-	return num_spaces + 1
-
-
-# noinspection PyBroadException
-def is_int(string):
-	try:
-		int(string)
-	except:
-		return False
-	return True
-
-
-def is_valid_coordinates(text):
-	if num_strings(text) != 2:
-		return False
-
-	strings = text.split()
-	for string in strings:
-		if is_int(string) is False:
-			return False
-
-	return True
 
 
 class Player(object):
@@ -222,99 +230,34 @@ class Player(object):
 		else:
 			print(Player.white_num_stones_capt)
 
-	def make_move(self, board):
-		user_input = input("Ok, %s, place your stone.\n" % self.colour)
-		while True:
-			if user_input.lower().find("pass") >= 0:
-				return
-			elif is_valid_coordinates(user_input):
-				coordinates = user_input.split()
-				# print("user_input = \"%s\"" % user_input)
-				# print("coordinates = %s\n" % coordinates)
-				row = int(coordinates[0])
-				col = int(coordinates[1])
-				if row > 18 or row < 0 or col > 18 or col < 0:
-					user_input = input("Invalid position, try again.\n")
-					continue
-				elif is_stone((row, col), board) is True:
-					user_input = input("Position occupied, try again.\n")
-					continue
-				else:
-					break
-			else:
-				user_input = input("Please input 'pass' or TWO numbers: row col\n")
-
-		board[row][col] = self.symbol
+	#inputs: board is a 2-d square array of size 19, pos is a tuple of coordinates
+	def make_move(self, board, pos):
+		col, row = pos
+		board[col][row] = self.symbol
+		#print(self.symbol)
+		#create iterable list to scan through
 		white_stones_played_iter = list(Player.white_stones_played)
 		black_stones_played_iter = list(Player.black_stones_played)
+
 		if self.colour == "black":
-			Player.black_stones_played.append((row, col))
+			Player.black_stones_played.append((col, row))
 			Player.black_stones_left -= 1
 			# print("Checking white liberties")
 			for pos in white_stones_played_iter:
-				Player.black_num_stones_capt += check_liberties(pos, board, Player.white_stones_played)
+				Player.black_num_stones_capt += check_liberties(board, pos, Player.white_stones_played)
 			# print("Checking black liberties")
 			for pos in black_stones_played_iter:
-				Player.white_num_stones_capt += check_liberties(pos, board, Player.black_stones_played)
+				Player.white_num_stones_capt += check_liberties(board, pos, Player.black_stones_played)
 		else:
-			Player.white_stones_played.append((row, col))
+			Player.white_stones_played.append((col, row))
 			Player.white_stones_left -= 1
 
 			# print("Checking black liberties")
 			for pos in black_stones_played_iter:
-				Player.white_num_stones_capt += check_liberties(pos, board, Player.black_stones_played)
+				Player.white_num_stones_capt += check_liberties(board, pos, Player.black_stones_played)
 			# print("Checking white liberties")
 			for pos in white_stones_played_iter:
-				Player.black_num_stones_capt += check_liberties(pos, board, Player.white_stones_played)
-
-"""
-black = Player("black")
-white = Player("white")
-
-print("\n\n\n")
-print("Hello! Welcome to Go! Please start now.")
-
-while black.black_stones_left > 0 and white.white_stones_left > 0:
-	if __debug__:
-		print_board(game_board)
-	remove_capture_indicators(game_board)
-	print("Player 1's turn. (%d stones left, %d stone(s) captured)" % (black.black_stones_left, black.black_num_stones_capt))
-	black.make_move(game_board)
-	if __debug__:
-		print_board(game_board)
-	remove_capture_indicators(game_board)
-	print("Player 2's turn. (%d stones left, %d stone(s) captured)" % (white.white_stones_left, white.white_num_stones_capt))
-	white.make_move(game_board)
-
-
-while black.black_stones_left > 0:
-	if __debug__:
-		print_board(game_board)
-	remove_capture_indicators(game_board)
-	print("Player 1's turn. (%d stones left, %d stone(s) captured)" % (black.black_stones_left, black.black_num_stones_capt))
-	black.make_move(game_board)
-
-
-while white.white_stones_left > 0:
-	if __debug__:
-		print_board(game_board)
-	remove_capture_indicators(game_board)
-	print("Player 2's turn. (%d stones left, %d stone(s) captured)" % (white.white_stones_left, white.white_num_stones_capt))
-	white.make_move(game_board)
-
-
-if __debug__:
-	print_board(game_board)
-
-if black.black_num_stones_capt == white.white_num_stones_capt:
-	print("Congratulations, you have tied")
-elif black.black_num_stones_capt > white.white_num_stones_capt:
-	print("Congratulations %s, you won!" % "PLayer 1")
-else:
-	print("Congratulations %s, you won!" % "Player 2")
-
-print("Game over!")
-"""
+				Player.black_num_stones_capt += check_liberties(board, pos, Player.white_stones_played)
 
 if __name__ == '__main__':
 	main()
